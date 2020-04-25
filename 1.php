9b603a5b84aba42a78d59ccc6303502d1b04ee67
crusherblack <53805953+crusherblack@users.noreply.github.com>
2020-04-25 20:22:21 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -11,28 +11,28 @@ echo "<button type='submit'>Generate 250 Serial Key</button>";
 
 echo "</form>";
 
-function generateRandomString($length = 4) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
+function generateRandomString($length = 4) { //function untuk generate random string degan digit sebanyak 4
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; //kasih tahu apa aja yg bisa dirandom
+    $charactersLength = strlen($characters); //panjang variable characters
+    $randomString = ''; //inialisasi random string kosong
+    for ($i = 0; $i < $length; $i++) { //perulangan 4 kali
+        $randomString .= $characters[rand(0, $charactersLength - 1)]; //sambung menyambung merandom string dari variable chracters hingga 0
     }
-    return $randomString; 
+    return $randomString; //return hasil
 }
 
 function tes(){
-   return generateRandomString().'-'.generateRandomString().'-'.generateRandomString().'-'.generateRandomString();
+   return generateRandomString().'-'.generateRandomString().'-'.generateRandomString().'-'.generateRandomString(); //format aja
 }
 
-function generate($jumlah){
+function generate($jumlah){ //perulangan sejumlah berapa banyak digenerate | krn soal katanya harus 250 jadi udah static value di input name qty
   for($i = 0; $i < $jumlah; $i++){
     echo tes().'<br>';  
   } 
 }
 
 
-if (!empty($_POST)){
+if (!empty($_POST)){ //validasi untuk cek ada post dan qty
 $jumlah = $_POST['qty'];
 
 }
@@ -41,4 +41,4 @@ else{
 }
 
 
-echo generate($jumlah);
+echo generate($jumlah); //hasil serial number yang diharapkan
